23242bc794ca74e7b44a962d1f16d420f95e15de
maurolepore <maurolepore@gmail.com>
2018-11-14 14:37:52 -0500

    As tibble.

--- data-raw/data.R
+++ data-raw/data.R
@@ -13,7 +13,9 @@ tree_ls <- pick_n(
   fgeo.data::luquillo_tree5_random,
   fgeo.data::luquillo_tree6_random,
   30
-)
+) %>%
+  map(as.tibble)
+
 tree5 <- tree_ls[[1]]
 tree6 <- tree_ls[[2]]
 use_data(tree5, tree6, overwrite = TRUE)
@@ -25,7 +27,9 @@ stem_ls <- pick_n(
   fgeo.data::luquillo_stem5_random,
   fgeo.data::luquillo_stem6_random,
   30
-)
+) %>%
+  map(as.tibble)
+
 stem5 <- stem_ls[[1]]
 stem6 <- stem_ls[[2]]
 use_data(stem5, stem6, overwrite = TRUE)
@@ -33,20 +37,22 @@ use_data(stem5, stem6, overwrite = TRUE)
 
 
 elevation <- fgeo.data::luquillo_elevation
+elevation$col <- as.tibble(elevation$col)
 use_data(elevation, overwrite = TRUE)
 
 
 
 vft_4quad <- fgeo.data::luquillo_vft_4quad %>%
-  dplyr::sample_n(500)
+  dplyr::sample_n(500) %>%
+  as.tibble()
 use_data(vft_4quad, overwrite = TRUE)
 
 
 
-taxa <- fgeo.data::luquillo_taxa
+taxa <- as.tibble(fgeo.data::luquillo_taxa)
 use_data(taxa, overwrite = TRUE)
 
 
 
-habitat <- fgeo.data::luquillo_habitat
+habitat <- as.tibble(fgeo.data::luquillo_habitat)
 use_data(habitat, overwrite = TRUE)
