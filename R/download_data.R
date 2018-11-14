bb739edd9d34bcf24c2289f82f6c646c5a5a922e
maurolepore <maurolepore@gmail.com>
2018-11-14 15:05:17 -0500

    New download_data().

---
+++ R/download_data.R
@@ -0,0 +1,25 @@
+#' Download data from fgeo.data.
+#'
+#' @param .data A string giving the name of the dataset to download. The name of
+#'   any dataset listed at
+#'   https://github.com/forestgeo/fgeo.data/tree/master/data (without the
+#'   extension).
+#'
+#' @return A dataset.
+#' @export
+#'
+#' @examples
+#' download_data("unique_id")
+download_data <- function(.data) {
+  tmp <- tempfile()
+  download.file(data_url(.data), tmp)
+
+  e <- new.env()
+  load(tmp, envir = e)
+
+  e[[.data]]
+}
+
+data_url <- function(x) {
+  paste0("https://github.com/forestgeo/fgeo.data/raw/master/data/", x, ".rda")
+}
