3cb3083230b3073f4136ff09901c209beafb8934
maurolepore <maurolepore@gmail.com>
2018-12-31 12:48:49 -0500

    Regression test existing extdata.

---
+++ tests/testthat/test-path_example.R
@@ -0,0 +1,16 @@
+context("path_example")
+
+expect_printed_output <- function(object, update = FALSE) {
+  file <- rlang::quo_text(rlang::enquo(object))
+
+  testthat::expect_known_output(
+    object, paste0("ref-", file),
+    print = TRUE, update = update
+  )
+
+  invisible(object)
+}
+
+test_that("extdata remains the same", {
+  expect_printed_output(path_example())
+})
