d61919b3f2e469b204d5eda52e3f84f97b3145aa
maurolepore <maurolepore@gmail.com>
2018-12-31 12:35:49 -0500

    New path_example() centralizes extdata/

---
+++ R/path_example.R
@@ -0,0 +1,20 @@
+#' Path to directory containing example data.
+#'
+#' @param path Path to a file (with extension) from inst/extdata/
+#'
+#' @return A string giving a path to an example.
+#'
+#' @examples
+#' path_example()
+#' str(read.csv(path_example("taxa.csv")))
+#'
+#' @family datasets
+#' @export
+path_example <- function(path = NULL) {
+  if (is.null(path)) {
+    dir(system.file("extdata", package = "fgeo.x"))
+  }
+  else {
+    system.file("extdata", path, package = "fgeo.x", mustWork = TRUE)
+  }
+}
