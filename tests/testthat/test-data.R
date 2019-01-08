99e45865e9b62e9348ddc5b80ed1f459cf481ef9
maurolepore <maurolepore@gmail.com>
2019-01-08 16:52:11 -0500

    Restore suggests on purrr and tibble to avoid warning about non-acii characters.

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,15 +1,19 @@
 context("data")
 
 test_that("data remains unchanged", {
+  skip_if_not_installed("purrr")
+  skip_if_not_installed("tibble")
+
   library(fgeo.x)
   pkg <- "fgeo.x"
-
-  datasets_chr <- utils::data(package = pkg)$results[, "Item"]
+  datasets_chr <-   utils::data(package = pkg)$results[, "Item"]
   datasets <- datasets_chr %>%
-    lapply(function(.x) get(.x, asNamespace(pkg))) %>%
-    stats::setNames(datasets_chr) %>%
+    purrr::map(~get(.x, asNamespace(pkg))) %>%
+    purrr::set_names(datasets_chr) %>%
+    purrr::map_if(is.data.frame, tibble::as.tibble) %>%
     # head of each list item
-    lapply(head)
+    purrr::map_if(Negate(is.data.frame), ~purrr::map(.x, head))
 
   expect_known_output(datasets, "ref-datasets", print = TRUE, update = TRUE)
 })
+
