859484aaa94fb4b90a778f2f3b17b008d0701d71
maurolepore <maurolepore@gmail.com>
2019-01-08 16:37:43 -0500

    Remove dependency on rlang.

--- tests/testthat/test-example_path.R
+++ tests/testthat/test-example_path.R
@@ -1,16 +1,8 @@
 context("example_path")
 
-expect_printed_output <- function(object, update = FALSE) {
-  file <- rlang::quo_text(rlang::enquo(object))
-
-  testthat::expect_known_output(
-    object, paste0("ref-", file),
-    print = TRUE, update = update
-  )
-
-  invisible(object)
-}
-
 test_that("extdata remains the same", {
-  expect_printed_output(example_path())
+  example_path <- example_path()
+  expect_known_output(
+    example_path, "ref-example_path", print = TRUE, update = FALSE
+  )
 })
