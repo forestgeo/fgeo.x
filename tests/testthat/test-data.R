175a843020d4c8c447cf186343fcd86b73845550
maurolepore <maurolepore@gmail.com>
2019-01-08 19:30:38 -0500

    Remove use of stats

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -6,7 +6,7 @@ test_that("data remains unchanged", {
   pkg <- "fgeo.x"
   datasets_chr <- utils::data(package = pkg)$results[, "Item"]
   datasets <- lapply(datasets_chr, function(x) get(x, asNamespace(pkg)))
-  datasets <- stats::setNames(datasets, datasets_chr)
+  names(datasets) <- datasets_chr
   datasets <- lapply(datasets, function(x) lapply(x, head))
 
   expect_known_output(datasets, "ref-datasets", print = TRUE, update = FALSE)
