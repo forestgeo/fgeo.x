601789fb9251f5f4c138702704c96a516f0110ad
maurolepore <maurolepore@gmail.com>
2019-01-08 20:04:27 -0500

    Simplify test.

--- cran-comments.md
+++ cran-comments.md
@@ -4,11 +4,15 @@
 * ubuntu 14.04 (on travis-ci), R 3.5.1
 * win-builder (devel and release)
 
+
 ## R CMD check results
 
 0 errors | 0 warnings | 1 note
 
 * This is a new release.
+
+NOTE: (devel and releaserelease)
+New submission 
 
 ## Downstream dependencies
 
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,13 +1,16 @@
 context("data")
 
 test_that("data remains unchanged", {
-  library(fgeo.x)
-
-  pkg <- "fgeo.x"
-  datasets_chr <- utils::data(package = pkg)$results[, "Item"]
-  datasets <- lapply(datasets_chr, function(x) get(x, asNamespace(pkg)))
-  names(datasets) <- datasets_chr
-  datasets <- lapply(datasets, function(x) lapply(x, head))
+  datasets <- list(
+    elevation,
+    habitat,
+    stem5,
+    stem6,
+    tree5,
+    tree6,
+    tree6_3species,
+    vft_4quad
+  )
 
   expect_known_output(datasets, "ref-datasets", print = TRUE, update = FALSE)
 })
